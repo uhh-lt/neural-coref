8c9c628d8ea1235b4d65be9f45710010e0fb7c57
Fynn Schröder <fschroeder@informatik.uni-hamburg.de>
2021-04-29 16:00:05 +0200

    redefined tuba11 splits to tuba10 with extra data

--- split_tuebadz_11.py
+++ split_tuebadz_11.py
@@ -1,25 +1,12 @@
 #!/usr/bin/env python3
 """
-For tuebadz 11 we create a custom split.
+Perform split to produce same test/dev set as tuba10 with slightly larger training set.
 """
 import sys
-import math
 
-# Tüba-DZ 11 has a total of 3816 documents
-# There are a couple of obvious ways to split them.
-# Following GerHotCoref we might use the first few docs as test and dev.
-# But we don't want to have any of the SemEval 2010 docs in our test sets.
-# That way we can use SemEval's singleton mentions for pre-training without worrying.
 f = open(sys.argv[1])
 
-
-total_size = 3816
-dev_size = 382
-test_size = 382
-train_size = total_size - dev_size - test_size
-
-assert dev_size + test_size + train_size == total_size
-splits = [(train_size, "train.german.tuebdz_gold_conll"), (dev_size, "dev.german.tuebdz_gold_conll"), (test_size, "test.german.tuebdz_gold_conll")]
+splits = [(727, "test.german.tuba11_gold_conll"), (727, "dev.german.tuba11_gold_conll"), (2362, "train.german.tuba11_gold_conll")]
 
 out_files = []
 for _, split in splits:
